de69280ce407fd7d91e3044f8ab3836babd955ed
YOOK DONGHYUN <aomorikaitou93@gmail.com>
2019-07-12 15:25:44 +0900

    2019-07-12 : add slf4j, logback code

--- guestbook/src/main/java/kr/or/connect/guestbook/intercepter/LogInterceptor.java
+++ guestbook/src/main/java/kr/or/connect/guestbook/intercepter/LogInterceptor.java
@@ -3,16 +3,25 @@ package kr.or.connect.guestbook.intercepter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter 
 {
+	private Logger logger = LoggerFactory.getLogger(this.getClass()); // 로거 객체를 생성
+	
 	/** preHandle: 컨트롤러 실행 전 */
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception 
 	{
-		System.out.println(handler.toString() + "를 호출했습니다.\n");
+		/* println()을 이용한 로그 출력 */
+		// System.out.println(handler.toString() + "를 호출했습니다.\n");
+		
+		/* 로거 객체를 이용한 로그 출력 */
+		logger.debug("{}를 호출했습니다.%n", handler.toString());
+		
 		return true;
 	}
 	
@@ -20,6 +29,10 @@ public class LogInterceptor extends HandlerInterceptorAdapter
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception 
 	{
-		System.out.println(handler.toString() + "가 종료되었습니다.\n" + modelAndView.getViewName() + "를 view로 사용합니다.\n\n" );
+		/* println()을 이용한 로그 출력 */
+		// System.out.println(handler.toString() + "가 종료되었습니다.\n" + modelAndView.getViewName() + "를 view로 사용합니다.\n\n" );
+	
+		/* 로거 객체를 이용한 로그 출력 */
+		logger.debug("{}가 종료되었습니다. {}를 view로 사용합니다.%n", handler.toString(), modelAndView.getViewName()); // 가변인자와 순서대로 매핑
 	}
 }
